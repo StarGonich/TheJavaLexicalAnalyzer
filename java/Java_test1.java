bfbadb17dc12e11e1999d6f32bf1a00557b58e13
Alexey Kiselev <alexey8837@gmail.com>
2024-04-15 15:49:34 +0300

    Принятый вариант

---
+++ java/Java_test1.java
@@ -0,0 +1,41 @@
+public class LexemeExample {
+    public static void main(String[] args) { //однострочный комментарий
+        public number = 0.0e+\u0031;
+        /*
+        Многострочный комментарий
+        Строка 2комментарий
+        Строка 3
+        */
+        String te\u0410xt = '\'';
+        String te\u0410xt = '\007';
+        if (number > 5) {
+            System.out.println(text);
+        } else {
+            System.out.println("Number is less than or equal to 5");
+        }
+        
+        for (int i = 0; i < number; i++) {
+            System.out.print(i + " ");
+        }
+
+        while (number > 0) {
+            number--;
+        }
+        
+        switch (number) {
+            case 0:
+                System.out.println("\nNumber is now 0");
+                break;
+            default:
+                System.out.println("\nNumber is not 0");
+        }
+        
+        int[] numbers = {1, 2, 3, 4, 5};
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+
+        a =>>> b;
+    }
+}
+//
